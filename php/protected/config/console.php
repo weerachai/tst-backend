b6ed59ae1645d97ac6e597e15291cca6853c22af
tst <admin@localhost>
2013-06-09 10:59:40 +0700

    udpate 2013-06-09

--- php/protected/config/console.php
+++ php/protected/config/console.php
@@ -2,6 +2,17 @@
 
 // This is the configuration for yiic console application.
 // Any writable CConsoleApplication properties can be configured here.
+
+$OPENSHIFT_MYSQL_DB_HOST = getenv("OPENSHIFT_MYSQL_DB_HOST");
+if (empty($OPENSHIFT_MYSQL_DB_HOST)) {
+   $OPENSHIFT_MYSQL_DB_HOST = 'localhost';
+   $OPENSHIFT_MYSQL_USERNAME = 'root';
+   $OPENSHIFT_MYSQL_PASSWORD = '1234';
+} else {
+   $OPENSHIFT_MYSQL_USERNAME = 'adminTbebzg2';
+   $OPENSHIFT_MYSQL_PASSWORD = 'lKkp4SIWiue9';
+}
+
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Console Application',
@@ -23,10 +34,10 @@ return array(
 		// uncomment the following to use a MySQL database
 	
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=backend',
+			'connectionString' => "mysql:host=$OPENSHIFT_MYSQL_DB_HOST;dbname=backend",
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '1234',
+			'username' =>  $OPENSHIFT_MYSQL_USERNAME,
+			'password' =>  $OPENSHIFT_MYSQL_PASSWORD,
 			'charset' => 'utf8',
 		),
 		
